f7178f69f021bef94fbad0f0ae09da3afec03c88
TimeCafeMarrakech <clubhub.mohammedia@gmail.com>
2026-06-10 02:39:01 +0100

    Update supabaseClient.tsx

--- app/supabaseClient.tsx
+++ app/supabaseClient.tsx
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
-// Initialize the secure, authenticated cloud channel tunnel
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);// Force update token log:  2:26:08.49 
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
